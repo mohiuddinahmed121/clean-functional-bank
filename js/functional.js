a4628b36858435f0e23ea1a57ae228ad9e085eda
mohiuddinahmed121 <mohiuddinahmed121@gmail.com>
2022-07-07 13:53:33 +0600

    commited

--- js/functional.js
+++ js/functional.js
@@ -5,18 +5,20 @@ function getInputValue(fieldId) {
     inputField.value = '';
     return value;
 }
+function getInnerTextValue(fieldId) {
+    const fieldTag = document.getElementById(fieldId);
+    const fieldValueInText = fieldTag.innerText;
+    const value = parseFloat(fieldValueInText);
+    return value;
+}
 function updateTotal(fieldId, amount) {
-    const totalTag = document.getElementById(fieldId);
-    const previousTotalInText = totalTag.innerText;
-    const previousTotal = parseFloat(previousTotalInText);
+    const previousTotal = getInnerTextValue(fieldId);
     const newTotal = previousTotal + amount;
-    totalTag.innerText = newTotal;
+    document.getElementById(fieldId).innerText = newTotal;
     return newTotal;
 }
 function updateBalance(amount, isAdding) {
-    const balanceTag = document.getElementById('balance-total');
-    const balanceInText = balanceTag.innerText;
-    const previousBalance = parseFloat(balanceInText);
+    const previousBalance = getInnerTextValue('balance-total');
     let newBalance;
     if (isAdding == true) {
         newBalance = previousBalance + amount;
@@ -24,7 +26,7 @@ function updateBalance(amount, isAdding) {
     else {
         newBalance = previousBalance - amount;
     }
-    balanceTag.innerText = newBalance;
+    document.getElementById('balance-total').innerText = newBalance;
 }
 document.getElementById('deposit-button').addEventListener('click', function () {
     const amount = getInputValue('deposit-input');
@@ -37,10 +39,9 @@ document.getElementById('deposit-button').addEventListener('click', function () 
 // handle withdraw
 document.getElementById('withdraw-button').addEventListener('click', function () {
     const amount = getInputValue('withdraw-input');
-    if (amount > 0) {
+    const balance = getInnerTextValue('balance-total')
+    if (amount > 0 && amount <= balance) {
         updateTotal('withdraw-total', amount);
         updateBalance(amount, false);
     }
-    updateTotal('withdraw-total', amount);
-    updateBalance(amount, false);
 });
